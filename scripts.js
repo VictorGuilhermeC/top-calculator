7b700283f809dc5abec9e973ecd6e0fd13a729fc
VictorGuilhermeC <67566818+VictorGuilhermeC@users.noreply.github.com>
2025-07-16 01:46:42 -0300

    Add event listener to update logic on display
    Event handlers connects numbers and allClear to output

--- scripts.js
+++ scripts.js
@@ -30,3 +30,47 @@ function operate(operator, num1, num2) {
       break;
   }
 }
+
+const display = document.querySelector("#display");
+let displayNum = display.innerText;
+
+const buttons = document.querySelector(".buttons");
+buttons.addEventListener("click", (event) => {
+  if (display.innerText[0] === "0") {
+    display.innerText = "";
+  }
+  switch (event.target.id) {
+    case "one":
+      display.innerText += "1";
+      break;
+    case "two":
+      display.innerText += "2";
+      break;
+    case "three":
+      display.innerText += "3";
+      break;
+    case "four":
+      display.innerText += "4";
+      break;
+    case "five":
+      display.innerText += "5";
+      break;
+    case "six":
+      display.innerText += "6";
+      break;
+    case "seven":
+      display.innerText += "7";
+      break;
+    case "eight":
+      display.innerText += "8";
+      break;
+    case "nine":
+      display.innerText += "9";
+      break;
+    case "zero":
+      display.innerText += "0";
+      break;
+    case "allClear":
+      display.innerText = 0;
+  }
+});
